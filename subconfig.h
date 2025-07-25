75ceb8d813ecf621b8b4c00885d2544c2e9823df
look4green <look4green@users.noreply.github.com>
2025-07-25 11:37:57 +0800

    Update subconfig.h

--- subconfig.h
+++ subconfig.h
@@ -4,9 +4,7 @@
 #include "utils.h"
 #include "arduinoFFT.h"
 #include <TFT_eSPI.h> 
-
 #include <ELECHOUSE_CC1101_ESP32DIV.h>
-
 #include <RCSwitch.h>
 #include <EEPROM.h>
 #include <stdio.h>
@@ -14,18 +12,14 @@
 #include <cstddef>
 #include <Wire.h>
 
-extern TFT_eSPI tft;
-extern PCF8574 pcf;
-
-
-// Define touchscreen pins (updated with your values)
+// Define touchscreen pins (confirmed values)
 #define XPT2046_CS    33  // Chip Select
 #define XPT2046_IRQ   27  // Interrupt (input-only, ensure compatibility)
 #define XPT2046_MOSI  22  // SPI MOSI
 #define XPT2046_MISO  19  // SPI MISO
 #define XPT2046_CLK   18  // SPI Clock
 
-
+extern TFT_eSPI tft;
 
 namespace replayat {
   void ReplayAttackSetup();
@@ -41,7 +35,5 @@ namespace subjammer {
   void subjammerSetup();
   void subjammerLoop();
 }
-
-
 
 #endif // SUBCONFIG_H
