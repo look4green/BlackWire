f72819d8ba42e55035451e4641a583fcb6d9b56e
look4green <look4green@users.noreply.github.com>
2025-08-02 16:26:06 +0800

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,60 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+
+// TFT & Touch
+TFT_eSPI tft = TFT_eSPI();
+SPIClass touchscreenSPI = SPIClass(VSPI);
+XPT2046_Touchscreen ts(33, 34);
+
+// Output Pins
+#define BUZZER_PIN 27     // Connect buzzer here
+#define LED_PIN    2      // Onboard LED on many ESP32 boards
+
+#define TS_MINX 300
+#define TS_MAXX 3800
+#define TS_MINY 300
+#define TS_MAXY 3800
+#define DISPLAY_WIDTH 240
+#define DISPLAY_HEIGHT 320
+
+void setup() {
+  Serial.begin(115200);
+
+  // TFT setup
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_GREEN);
+  tft.setTextSize(2);
+  tft.drawString("System Ready!", 10, 10);
+
+  // Touchscreen setup
+  touchscreenSPI.begin(25, 35, 32, 33);
+  ts.begin(touchscreenSPI);
+  ts.setRotation(1);
+
+  // Output setup
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop() {
+  if (ts.touched()) {
+    TS_Point p = ts.getPoint();
+    int x = map(p.x, TS_MINX, TS_MAXX, 0, DISPLAY_WIDTH);
+    int y = map(p.y, TS_MINY, TS_MAXY, 0, DISPLAY_HEIGHT);
+
+    // Visual feedback
+    tft.fillCircle(x, y, 5, TFT_RED);
+
+    // Simulate press: LED flash & buzzer beep
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(100);
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(50);  // debounce cooldown
+  }
+}
